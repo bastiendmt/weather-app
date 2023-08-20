bfa0e4359122fa4b329810dcba6ad42c5dc9ed72
Bastien Dumont <bastienxs@gmail.com>
2023-08-20 12:42:09 +0200

    more stats

--- app/index.js
+++ app/index.js
@@ -73,6 +73,58 @@ export default function App() {
             </View>
           )}
         </View>
+        {/* forecast section */}
+        <View className='mx-4 flex justify-around flex-1 mb-2'>
+          {/* location */}
+          <Text className='text-white text-center text-2xl font-bold'>
+            London,
+            <Text className='text-lg font-semibold text-gray-300'>UK</Text>
+          </Text>
+          {/* weather image */}
+          <View className='flex-row justify-center'>
+            <Image
+              source={require('../assets/images/partlycloudy.png')}
+              className='w-52 h-52'
+            />
+          </View>
+          {/* degree celcius */}
+          <View className='space-y-2'>
+            <Text className='text-center font-bold text-white text-6xl ml-5'>
+              21&#176;
+            </Text>
+            <Text className='text-center  text-white text-xl tracking-widest'>
+              Party cloudy
+            </Text>
+          </View>
+          {/* other stats */}
+          <View className='flex-row justify-between mx-4'>
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require('../assets/icons/wind.png')}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>22km</Text>
+            </View>
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require('../assets/icons/drop.png')}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>23%</Text>
+            </View>
+            <View className='flex-row space-x-2 items-center'>
+              <Image
+                source={require('../assets/icons/sun.png')}
+                className='h-6 w-6'
+              />
+              <Text className='text-white font-semibold text-base'>
+                6:05 AM
+              </Text>
+            </View>
+          </View>
+        </View>
+
+        {/* forecast for next days */}
       </SafeAreaView>
     </View>
   );
